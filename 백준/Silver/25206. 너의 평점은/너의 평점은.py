fe90a87aef7a690810892bc135fe1172ec21c314
joosewon <100904315+wntpdnjs@users.noreply.github.com>
2024-09-13 19:12:34 +0900

    [Silver V] Title: 너의 평점은, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.py
@@ -0,0 +1,22 @@
+
+grade = {
+    "A+":4.5,
+    "A0": 4.0,
+    "B+":3.5,
+    "B0":3.0,
+    "C+":2.5,
+    "C0":2.0,
+    "D+":1.5,
+    "D0":1.0,
+    "F":0.0}
+sum = 0.0
+total_num = 0
+for i in range (20):
+    a,b,c = input().split()
+    if c == "P" :
+        pass
+    else : 
+        b=float(b)
+        sum += (b*float(grade[c]))
+        total_num += b
+print("%.5f" %(sum/total_num))
